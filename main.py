10bc5621ab9114d38253f443746f79d2b06d4565
weslley moura <weslley.data@gmail.com>
2022-07-19 14:29:20 -0300

    teste lib pubsub_v1

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 from google.cloud import pubsub_v1
 
 publisher = pubsub_v1.PublisherClient()
+project_id = "sandbox-testes"
+topic_id = "sandbox-testes-topic"
+topic_path = publisher.topic_path(project_id, topic_id)
+
+print("topic_path")
