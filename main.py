e528b0692f8244e47e43dd6a4fda6ec81547790f
Weslley Moura <weslley.data@gmail.com>
2022-08-01 22:04:34 -0300

    loop de publicacao

--- main.py
+++ main.py
@@ -5,4 +5,9 @@ project_id = "sandbox-testes"
 topic_id = "sandbox-testes-topic"
 topic_path = publisher.topic_path(project_id, topic_id)
 
-print(f"Nome do projeto é: {topic_path}")
+#print(f"Nome do projeto é: {topic_path}")
+
+for i in range(1, 10):
+    mensagem = "Número: {}".format(i)
+    mensagem_codificada = mensagem.encode("utf-8")
+    publisher.publish(topic_path, mensagem_codificada)
